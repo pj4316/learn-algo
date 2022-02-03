9940de1e18fc1cd2f8da1533d70255f7c9e14c4f
max lee (Max) <pj4316@naver.com>
2022-02-03 15:43:46 +0900

    Update MaximumSubarray.kt

--- 2022-02-03/MaximumSubarray.kt
+++ 2022-02-03/MaximumSubarray.kt
@@ -1,18 +1,15 @@
 /**
 * 연속적인 배열만 보기 때문에, 최대 가능한 합계 = max(sum of previous sub array + current, current)
 */
-
 import kotlin.math.max
 
 class Solution {
     fun maxSubArray(nums: IntArray): Int {
-        val result = nums.toMutableList();
         var maxValue = Int.MIN_VALUE;
-        nums.forEachIndexed { index, i ->
-            val sums = if(index>0) result[index-1] + i else i;
-            val maxNum = max(sums,i);
-            maxValue = max(maxValue, maxNum);
-            result[index] = maxNum
+        var current = 0;
+        nums.forEach { i ->
+            current = max(current + i,i);
+            maxValue = max(maxValue, current);
         }
         return maxValue;
     }
