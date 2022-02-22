dbf2c594b04c1e0173561523c76a7fee9a5dcbb8
max lee (Max) <pj4316@naver.com>
2022-02-22 18:47:39 +0900

    Create 신고이메일받기.kt

---
+++ 2022-02-22/신고이메일받기.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+        val answer = IntArray(id_list.size) { 0 }
+
+        val reporterMap = mutableMapOf<String, Set<String>>()
+        val reporteeMap = mutableMapOf<String, Set<String>>()
+        report.forEach {
+            val ids = it.split(" ");
+
+            val reporters = mutableSetOf<String>()
+            if(reporterMap.contains(ids[0])) reporters.addAll(reporterMap.getOrDefault(ids[0], emptySet()))
+            reporters.add(ids[1])
+
+            reporterMap[ids[0]] = reporters
+
+            val reportees = mutableSetOf<String>()
+            if(reporteeMap.contains(ids[1])) reportees.addAll(reporteeMap.getOrDefault(ids[1], emptySet()))
+            reportees.add(ids[0])
+
+            reporteeMap[ids[1]] = reportees
+        }
+
+        for(i in id_list.indices) {
+            answer[i] = reporterMap
+                .getOrDefault(id_list[i], emptySet()).count { reporteeMap.getOrDefault(it, emptySet()).size >= k };
+        }
+
+        return answer
+    }
+}
